a6832cb6ba55a9a36796afd8f409253b505e4a79
LueRader <laurin.kirsch@gmail.com>
2017-06-25 16:46:04 +0200

    Complete overhaul of the file to make it work
    Quick implementation of an any class, which only works for member pointers.

--- Cpp/hijack.cpp
+++ Cpp/hijack.cpp
@@ -1,52 +1,71 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <memory>
 
-namespace Y {
-	class A {
-		std::string s;
-		int x;
-		int y;
-		void f() { std::cout << "whoops" << std::endl; }
-	public:
-		void print_xy() {
-			std::cout << x << ' ' << y << ' ' << s << '\n';
-		}
-	};
-}
 
-template<int>
-struct num {};
-
-template<class T, class C>
-using PMember = T(C::*);
-
-template<class T, class C, int N>
-T& hijack(C&, num<N>);
-
-template<class T, class C>
-struct Wrapper {
-	template <PMember<T, C> pf, int N>
-	struct Hijack {
-		template<class Ts, class Cs>
-		friend Ts& hijack(Cs& s, num<N>) {
-			return s.*pf;
-		}
-	};
+class A {
+public:
+    void print() {
+        std::cout << x << ' ' << y << ' ' << s << '\n';
+    }
+private:
+    int x, y;
+    std::string s;
+  void f(int) { std::cout << "whoops" << std::endl; }
 };
 
 
-template struct Wrapper<int, Y::A>::Hijack<&Y::A::x, 0>;
-template struct Wrapper<int, Y::A>::Hijack<&Y::A::y, 1>;
-template struct Wrapper<std::string, Y::A>::Hijack<&Y::A::s, 2>;
+template<class T, class C>
+using Mem = T (C::*);
 
+class any {
+    struct base {
+        virtual ~base() {}
+    };
+
+    template<class T, class C>
+    struct derived : public base {
+        Mem<T,C> m;
+        derived(Mem<T,C> mem) : m{mem} {}
+        T& get(C& c) {
+            return c.*m;
+        }
+    };
+    
+public:
+    template<class T, class C>
+    T& get(C&& c) {
+        return static_cast<derived<T,typename std::remove_reference<C>::type>*>(holder_.get())
+            ->get(std::forward<C>(c));
+    }
+    
+    template<class T, class C>
+    any(Mem<T,C> mem) : holder_{new derived<T, C>{mem}} {}
+
+
+    std::shared_ptr<base> holder_;
+
+};
+
+std::vector<any> hijack();
+
+template<class... Ts>
+struct Wrapper {
+    template <Ts... pf>
+    struct Hijack {
+        friend std::vector<any> hijack() {
+            return std::vector<any>{any{pf}...};
+        }
+    };
+};
+
+template struct Wrapper<Mem<int,A>, Mem<int,A>, Mem<std::string,A>>::Hijack<&A::x, &A::y, &A::s>;
 
 int main() {
-	Y::A a;;
-	int& x = hijack<int>(a, num<0>{});
-	x = 10;
-	int& y = hijack<int>(a, num<1>{});
-	y = 11;
-	auto& s = hijack<std::string>(a, num<2>{});
-	s = "AYY";
-	a.print_xy();
+    A a;
+    auto arr = std::move(hijack());
+    arr[0].get<int>(a) = 10;
+    arr[1].get<int>(a) = 20;
+    arr[2].get<std::string>(a) = "Ayyy";
+    a.print();
 }
